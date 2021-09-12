dae98d3cf91fb78ffe9c157d32ac82fb2d5a1f9c
SammY2008 <88900622+SammY2008@users.noreply.github.com>
2021-09-11 19:23:01 -0500

    Add files via upload

---
+++ Iron.js
@@ -0,0 +1,29 @@
+class Iron{
+	constructor(x,y,width,height)
+	{
+	var options={
+		'restitution':0.8,
+		'friction':3,
+		'density':30
+	};
+    this.body = Bodies.rectangle(800, 500, 20, 20, options);
+    this.width = 100;
+    this.height = 100;
+    World.add(world, this.body);
+  };
+  display(){
+    var pos = this.body.position;
+    var angle = this.body.angle;
+
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    fill('black')
+    rectMode(CENTER)
+    rect(0, 0, this.width, this.height);
+    pop();
+
+
+    }
+
+}
